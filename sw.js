c829ad1558b51f19c077faddad466ddc5f72d580
Indoor-Cycling <ralphdawkins@gmail.com>
2026-09-23 16:01:05 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    not an older one.
    Bump VERSION below whenever you upload changed files. That forces every
    installed copy to re-download the whole app on its next launch. */
-var VERSION = 's60-v42';
+var VERSION = 's60-v43';
 var CACHE = 'saturday60-' + VERSION;
 
 var ASSETS = [
